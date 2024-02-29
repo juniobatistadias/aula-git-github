c9aefd9d9321331f83aeb1ba057d10f88d94c39f
juniobatistadias <102702600+juniobatistadias@users.noreply.github.com>
2024-02-29 10:08:28 -0300

    Update teste.java

--- teste/src/teste/teste.java
+++ teste/src/teste/teste.java
@@ -1,10 +1,8 @@
 package teste;
-
-public class teste {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class Teste {
+public static void main(String[] args) {
+int num1 = 3, num2 = 5, soma;
+soma = num1 + num2;
+System.out.println(soma);
+}
 }
